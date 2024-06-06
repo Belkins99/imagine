fe6b797b9d56a9a5fade23cfe011a9a0c6af1d46
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-06-06 11:44:08 +0000

    Math in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -62,4 +62,37 @@ const bottle1 = {
   for(let i = 0; i <= 5; i++ ) {
    console.log ('We did it!', i)
   }
-  
+
+  // Functions
+// Defining a function
+function login(username, password) {
+  // Validatemusername and passwordath
+  if (!username || !password){
+    return 'Username or password not provided';
+  }
+  // Verify username and password
+  if(username == 'Belkins' && password == '1234')  {
+    return 'User is logged in';} 
+  else {
+    return 'Invalid username or password';
+  }
+} 
+// Invoking a function
+login('Belkins', );
+
+// Basic Arithmetic Operations
+11 + 12;
+1 + 0.5;
+43 - 12
+45 / 7;
+5 * 3;
+14 & 3;
+
+Math.floor(45 / 24);
+45 %  24
+Math.random() * 1000;
+Math.max(34,32,37)
+2*(3+4)-5/2
+
+// the use of Num()
+Number('2')+2
